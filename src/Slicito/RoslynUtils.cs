652b6714463290585b2ccd89e64d759ddd401fe9
Robert Husák <robert@husak.cloud>
2022-05-16 15:07:02 +0200

    Implement loading Roslyn projects from disk

---
+++ src/Slicito/RoslynUtils.cs
@@ -0,0 +1,39 @@
+﻿namespace Slicito;
+
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+
+public static class RoslynUtils
+{
+    static RoslynUtils()
+    {
+        if (!MSBuildLocator.IsRegistered)
+        {
+            var sdks = MSBuildLocator.QueryVisualStudioInstances();
+
+            var firstNet5sdk = sdks.FirstOrDefault(sdk => sdk.Version.Major == 5);
+            if (firstNet5sdk != null)
+            {
+                // Currently, .NET 5 SDK is needed (as explained in https://github.com/dotnet/interactive/issues/1985)
+                MSBuildLocator.RegisterInstance(firstNet5sdk);
+            }
+            else if (sdks.Any())
+            {
+                var bestSdk = sdks.First();
+                Console.WriteLine($"Warning: No .NET 5 SDK found, registering '{bestSdk.MSBuildPath}' instead.");
+            }
+            else
+            {
+                Console.WriteLine($"Warning: No .NET SDK found, loading Roslyn projects may not work correctly.");
+            }
+        }
+    }
+
+    public static Task<Project> OpenProjectAsync(string path)
+    {
+        using var workspace = MSBuildWorkspace.Create();
+
+        return workspace.OpenProjectAsync(path);
+    }
+}
