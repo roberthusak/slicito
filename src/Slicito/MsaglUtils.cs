aeee5d24fe525760ead8558b46cdb62a9ec7a44b
Robert Husák <robert@husak.cloud>
2022-05-16 17:42:25 +0200

    Implement basic export to SVG

---
+++ src/Slicito/MsaglUtils.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.Msagl.Core.Geometry;
+using Microsoft.Msagl.Core.Geometry.Curves;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.Layout.Layered;
+using Microsoft.Msagl.Miscellaneous;
+using System.Diagnostics;
+
+namespace Slicito;
+
+public static class MsaglUtils
+{
+    public static IHtmlContent RenderSvg(Graph graph)
+    {
+        if (graph.GeometryGraph == null)
+        {
+            CalculateDefaultLayout(graph);
+        }
+
+        using var ms = new MemoryStream();
+        var svgWriter = new SvgGraphWriter(ms, graph);
+        svgWriter.Write();
+
+        ms.Position = 0;
+        var reader = new StreamReader(ms);
+        var svgString = reader.ReadToEnd();
+
+        return new HtmlString(svgString);
+    }
+
+    private static void CalculateDefaultLayout(Graph graph)
+    {
+        Debug.Assert(graph.GeometryGraph == null);
+
+        graph.CreateGeometryGraph();
+
+        foreach (var n in graph.Nodes)
+        {
+            n.GeometryNode.BoundaryCurve = CurveFactory.CreateRectangleWithRoundedCorners(60, 40, 3, 2, new Point(0, 0));
+
+            n.Label.Width = n.Width * 0.6;
+            n.Label.Height = 40;
+            n.Label.Text = n.Id;
+        }
+
+        foreach (var e in graph.Edges)
+        {
+            e.Label.GeometryLabel.Width = 140;
+            e.Label.GeometryLabel.Height = 60;
+        }
+
+        LayoutHelpers.CalculateLayout(graph.GeometryGraph, new SugiyamaLayoutSettings(), null);
+    }
+}
