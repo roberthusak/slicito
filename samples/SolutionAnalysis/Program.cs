59aa54e35fad0e896b05368c23da3f13e17341ee
Robert Husák <robert@husak.cloud>
2022-05-17 11:29:48 +0200

    Add sample displaying of symbol hierarchy

--- samples/SolutionAnalysis/Program.cs
+++ samples/SolutionAnalysis/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Slicito;
+using SolutionAnalysis;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+
+var project = await RoslynUtils.OpenProjectAsync(@"C:\datamole\repos\personal\benchmarkdotnet-sample\BenchmarkDotNetSample.csproj");
+var compilation = await project.GetCompilationAsync();
+
+var graphVisitor = new SymbolGraphVisitor();
+graphVisitor.Visit(compilation!.SourceModule);
+
+// Display SVG in the default application
+
+var svg = MsaglUtils.RenderSvg(graphVisitor.Graph);
+
+var svgPath = Path.GetFullPath("schema.svg");
+using (var writer = new StreamWriter(svgPath))
+{
+    svg.WriteTo(writer, HtmlEncoder.Default);
+}
+
+var process = new Process
+{
+    StartInfo = new ProcessStartInfo(svgPath)
+    {
+        UseShellExecute = true
+    }
+};
+process.Start();
---
+++ samples/SolutionAnalysis/SymbolGraphVisitor.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.Msagl.Drawing;
+
+namespace SolutionAnalysis;
+
+class SymbolGraphVisitor : SymbolVisitor
+{
+    public Graph Graph { get; } = new Graph("graph");
+
+    private Stack<ISymbol> SymbolStack { get; } = new();
+
+    public override void DefaultVisit(ISymbol symbol)
+    {
+        var node = Graph.AddNode(GetNodeIdFromSymbol(symbol));
+        node.LabelText = GetLabelFromSymbol(symbol);
+
+        if (SymbolStack.Count > 0)
+        {
+            Graph.AddEdge(
+                GetNodeIdFromSymbol(SymbolStack.Peek()),
+                "",
+                GetNodeIdFromSymbol(symbol));
+        }
+    }
+
+    public override void VisitModule(IModuleSymbol symbol)
+    {
+        DefaultVisit(symbol);
+
+        SymbolStack.Push(symbol);
+        symbol.GlobalNamespace.Accept(this);
+        SymbolStack.Pop();
+    }
+
+    public override void VisitNamedType(INamedTypeSymbol symbol)
+    {
+        DefaultVisit(symbol);
+
+        SymbolStack.Push(symbol);
+        VisitAll(symbol.GetMembers());
+        SymbolStack.Pop();
+    }
+
+    public override void VisitNamespace(INamespaceSymbol symbol)
+    {
+        DefaultVisit(symbol);
+
+        SymbolStack.Push(symbol);
+        VisitAll(symbol.GetMembers());
+        SymbolStack.Pop();
+    }
+
+    private void VisitAll(IEnumerable<ISymbol> symbols)
+    {
+        foreach (var symbol in symbols)
+        {
+            symbol.Accept(this);
+        }
+    }
+
+    // FIXME
+    private string GetNodeIdFromSymbol(ISymbol symbol) =>
+        symbol.GetHashCode().ToString();
+
+    private string GetLabelFromSymbol(ISymbol symbol) =>
+        string.IsNullOrEmpty(symbol.MetadataName) ? symbol.GetType().Name : symbol.MetadataName;
+}
