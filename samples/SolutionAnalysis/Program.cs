6ec5e03065d5a783d299499e9911431b134a5c75
Robert Husák <robert@husak.cloud>
2022-05-17 12:39:47 +0200

    Simplify analysis sample by removing SymbolVisitor

--- samples/SolutionAnalysis/Program.cs
+++ samples/SolutionAnalysis/Program.cs
@@ -1,18 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.Msagl.Drawing;
 using Slicito;
-using SolutionAnalysis;
 using System.Diagnostics;
 using System.Text.Encodings.Web;
 
 var project = await RoslynUtils.OpenProjectAsync(@"C:\datamole\repos\personal\benchmarkdotnet-sample\BenchmarkDotNetSample.csproj");
 var compilation = await project.GetCompilationAsync();
 
-var graphVisitor = new SymbolGraphVisitor();
-graphVisitor.Visit(compilation!.SourceModule);
+var graph = new Graph("graph");
+
+// FIXME
+string GetNodeIdFromSymbol(ISymbol symbol) =>
+    symbol.GetHashCode().ToString();
+
+string GetLabelFromSymbol(ISymbol symbol) =>
+    string.IsNullOrEmpty(symbol.MetadataName) ? symbol.GetType().Name : symbol.MetadataName;
+
+foreach (var symbol in compilation!.GetSymbolsWithName(_ => true, SymbolFilter.All))
+{
+    var node = graph.AddNode(GetNodeIdFromSymbol(symbol));
+    node.LabelText = GetLabelFromSymbol(symbol);
+
+    if (symbol.ContainingSymbol != null)
+    {
+        graph.AddEdge(
+            GetNodeIdFromSymbol(symbol.ContainingSymbol),
+            "",
+            GetNodeIdFromSymbol(symbol));
+    }
+}
 
 // Display SVG in the default application
 
-var svg = MsaglUtils.RenderSvg(graphVisitor.Graph);
+var svg = MsaglUtils.RenderSvg(graph);
 
 var svgPath = Path.GetFullPath("schema.svg");
 using (var writer = new StreamWriter(svgPath))
