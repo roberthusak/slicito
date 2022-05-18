8743dfde0e525e0504c2786dad2a82086f88a0bc
Robert Husák <robert@husak.cloud>
2022-05-18 16:36:56 +0200

    Add sample on displaying nested symbol hierarchy

--- samples/SolutionAnalysis/Program.cs
+++ samples/SolutionAnalysis/Program.cs
@@ -2,21 +2,54 @@
 using Microsoft.Msagl.Drawing;
 using Slicito;
 using SolutionAnalysis;
+using System.Diagnostics;
 
 var project = await RoslynUtils.OpenProjectAsync(args[0]);
 var compilation = await project.GetCompilationAsync();
 
 var graph = new Graph("graph");
 
-foreach (var symbol in compilation!.GetSymbolsWithName(_ => true, SymbolFilter.All))
+foreach (var symbol in compilation!.GetSymbolsWithName(_ => true, SymbolFilter.Namespace | SymbolFilter.Type))
 {
-    graph.AddSymbolAsNode(symbol);
+    AddSymbol(graph, symbol);
+}
 
-    if (symbol.ContainingSymbol != null)
+var svg = graph.RenderToSvg(LayoutOrientation.Vertical);
+Utils.SaveSvgAndOpen(svg);
+
+
+Subgraph AddSymbol(Graph graph, ISymbol symbol)
+{
+    if (symbol.ContainingSymbol == null)
     {
-        graph.AddEdgeBetweenSymbols(symbol.ContainingSymbol, symbol);
+        return AddSubgraph(graph, graph.RootSubgraph, symbol);
+    }
+    else
+    {
+        if (!graph.SubgraphMap.TryGetValue(symbol.ContainingSymbol.GetNodeId(), out var containingSubgraph))
+        {
+            containingSubgraph = AddSymbol(graph, symbol.ContainingSymbol);
+        }
+
+        return AddSubgraph(graph, containingSubgraph, symbol);
     }
 }
 
-var svg = graph.RenderToSvg();
-Utils.SaveSvgAndOpen(svg);
+Subgraph AddSubgraph(Graph graph, Subgraph containingSubgraph, ISymbol symbol)
+{
+    if (!graph.SubgraphMap.TryGetValue(symbol.GetNodeId(), out var subgraph))
+    {
+        subgraph = new Subgraph(symbol.GetNodeId())
+        {
+            LabelText = symbol.GetNodeLabelText()
+        };
+
+        containingSubgraph.AddSubgraph(subgraph);
+
+        // FIXME Hack to force SubgraphMap refresh
+        var retrievedNode = graph.AddNode(subgraph.Id);
+        Debug.Assert(ReferenceEquals(subgraph, retrievedNode));
+    }
+
+    return subgraph;
+}
