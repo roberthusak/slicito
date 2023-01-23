1d294f91fc127cbb4150c387b9c8e19a02e73ce8
Robert Husák <robert@husak.cloud>
2023-01-23 18:49:22 +0100

    Implement basic optimization of relations
    Group together incoming and outgoing pairs for each element.

--- src/Slicito/Abstractions/Relation_Generic.Builder.cs
+++ src/Slicito/Abstractions/Relation_Generic.Builder.cs
@@ -1,39 +1,57 @@
-using System.Collections.Immutable;
-
 namespace Slicito.Abstractions;
 
 public partial class Relation<TSourceElement, TTargetElement, TData>
 {
     public class Builder : IRelation<TSourceElement, TTargetElement, TData>
     {
-        private readonly List<IPair<TSourceElement, TTargetElement, TData>> _pairs = new();
+        private readonly Dictionary<TSourceElement, List<IPair<TSourceElement, TTargetElement, TData>>> _elementsToOutgoingMap = new();
+        private readonly Dictionary<TTargetElement, List<IPair<TSourceElement, TTargetElement, TData>>> _elementsToIncomingMap = new();
 
-        public IEnumerable<IPair<TSourceElement, TTargetElement, TData>> Pairs => _pairs;
+        public IEnumerable<IPair<TSourceElement, TTargetElement, TData>> Pairs => _elementsToOutgoingMap.Values.SelectMany(l => l);
 
         public IRelation<TSourceElement, TTargetElement, TData> AsRelation() => this;
 
-        public Builder Add(TSourceElement source, TTargetElement target, TData data)
-        {
-            _pairs.Add(new Pair<TSourceElement, TTargetElement, TData>(source, target, data));
-
-            return this;
-        }
+        public Builder Add(TSourceElement source, TTargetElement target, TData data) =>
+            Add(Pair.Create(source, target, data));
 
         public Builder Add(IPair<TSourceElement, TTargetElement, TData> pair)
         {
-            _pairs.Add(pair);
+            GetOrCreateList(_elementsToOutgoingMap, pair.Source).Add(pair);
+            GetOrCreateList(_elementsToIncomingMap, pair.Target).Add(pair);
 
             return this;
+
+            static List<IPair<TSourceElement, TTargetElement, TData>>
+                GetOrCreateList<TElement>(
+                    Dictionary<TElement, List<IPair<TSourceElement, TTargetElement, TData>>> dictionary,
+                    TElement element)
+                where TElement : notnull
+            {
+                if (dictionary.TryGetValue(element, out var list))
+                {
+                    return list;
+                }
+                else
+                {
+                    list = new();
+                    dictionary[element] = list;
+
+                    return list;
+                }
+            }
         }
 
         public Builder AddRange(IEnumerable<IPair<TSourceElement, TTargetElement, TData>> pairs)
         {
-            _pairs.AddRange(pairs);
+            foreach (var pair in pairs)
+            {
+                Add(pair);
+            }
 
             return this;
         }
 
         public Relation<TSourceElement, TTargetElement, TData> Build() =>
-            new(_pairs.ToImmutableArray());
+            new(_elementsToOutgoingMap, _elementsToIncomingMap);
     }
 }
