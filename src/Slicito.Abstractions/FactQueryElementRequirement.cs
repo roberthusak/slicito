2d94c8e4633d54a0bc383801e8c0b2a3d8c24bb2
Robert Husák <robert@husak.cloud>
2024-05-14 23:23:20 +0200

    Correct type of element and link filter in fact queries

--- src/Slicito.Abstractions/FactQueryElementRequirement.cs
+++ src/Slicito.Abstractions/FactQueryElementRequirement.cs
@@ -1,10 +1,10 @@
 namespace Slicito.Abstractions;
 
-public sealed class FactQueryElementRequirement(IElementKind kind, bool returnAll, Action<IElement>? filter = null)
+public sealed class FactQueryElementRequirement(IElementKind kind, bool returnAll, Predicate<IElement>? filter = null)
 {
     public IElementKind Kind { get; } = kind;
 
     public bool ReturnAll { get; } = returnAll;
 
-    public Action<IElement>? Filter { get; } = filter;
+    public Predicate<IElement>? Filter { get; } = filter;
 }
--- src/Slicito.Abstractions/FactQueryRelationRequirement.cs
+++ src/Slicito.Abstractions/FactQueryRelationRequirement.cs
@@ -1,10 +1,10 @@
 namespace Slicito.Abstractions;
 
-public sealed class FactQueryRelationRequirement(IRelationKind kind, bool returnAll, Action<ILink>? filter = null)
+public sealed class FactQueryRelationRequirement(IRelationKind kind, bool returnAll, Predicate<ILink>? filter = null)
 {
     public IRelationKind Kind { get; } = kind;
 
     public bool ReturnAll { get; } = returnAll;
 
-    public Action<ILink>? Filter { get; } = filter;
+    public Predicate<ILink>? Filter { get; } = filter;
 }
