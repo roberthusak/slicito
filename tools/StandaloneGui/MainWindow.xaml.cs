58e0bbe7756fdafa08b59fac29e392dd312ddf50
Robert Husák <robert@husak.cloud>
2024-08-06 20:20:32 +0200

    Show all solution elements in StandaloneGui

--- tools/StandaloneGui/MainWindow.xaml.cs
+++ tools/StandaloneGui/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,6 +9,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.CodeAnalysis.MSBuild;
+
+using Slicito.Abstractions;
+using Slicito.DotNet;
+
 namespace StandaloneGui;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -18,5 +23,37 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void Window_LoadedAsync(object sender, RoutedEventArgs e)
+    {
+        progressBar.IsIndeterminate = true;
+
+        var args = Environment.GetCommandLineArgs();
+        var solution = await MSBuildWorkspace.Create().OpenSolutionAsync(args[1]);
+
+        var provider = new DotNetFactProvider(solution);
+
+        var query = new FactQuery(
+            [
+                new FactQueryElementRequirement(DotNetElementKind.Solution, includeChildless: false),
+                new FactQueryElementRequirement(DotNetElementKind.Project, includeChildless: false),
+                new FactQueryElementRequirement(DotNetElementKind.Namespace, includeChildless: false),
+                new FactQueryElementRequirement(DotNetElementKind.Type, includeChildless: true),
+            ],
+            [
+                new FactQueryRelationRequirement(DotNetRelationKind.SolutionContains, includeChildless: false),
+                new FactQueryRelationRequirement(DotNetRelationKind.ProjectContains, includeChildless: false),
+                new FactQueryRelationRequirement(DotNetRelationKind.NamespaceContains, includeChildless: false)
+            ]);
+
+        var result = await provider.QueryAsync(query);
+
+        foreach (var element in result.Elements)
+        {
+            treeView.Items.Add(new TreeViewItem { Header = element.Id });
+        }
+
+        progressBar.IsIndeterminate = false;
     }
 }
