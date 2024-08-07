421cc51c002a05e8f5aa9c2326dc9c07824d4ff5
Robert Husák <robert@husak.cloud>
2024-08-07 15:51:55 +0200

    Load controller dynamically in StandaloneGui

--- tools/StandaloneGui/MainWindow.xaml.cs
+++ tools/StandaloneGui/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,20 +22,38 @@ namespace StandaloneGui;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly string _solutionPath;
+    private readonly string _controllersPath;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        var args = Environment.GetCommandLineArgs();
+        _solutionPath = args[1];
+        _controllersPath = args[2];
     }
 
     private async void Window_LoadedAsync(object sender, RoutedEventArgs e)
     {
-        var args = Environment.GetCommandLineArgs();
-        var solution = await MSBuildWorkspace.Create().OpenSolutionAsync(args[1]);
+        var solution = await MSBuildWorkspace.Create().OpenSolutionAsync(_solutionPath);
 
         var provider = new DotNetFactProvider(solution);
 
-        var controller = new DotNetTreeController(provider);
+        var controller = LoadController(provider);
 
         _contentPanel.Children.Add(new ToolPanel(controller));
+    }
+
+    private IController LoadController(DotNetFactProvider provider)
+    {
+        var assembly = Assembly.LoadFrom(_controllersPath);
+
+        var type = assembly.GetTypes().Single(t => typeof(IController).IsAssignableFrom(t));
+
+        var controller = Activator.CreateInstance(type, provider)
+            ?? throw new ApplicationException($"Unable to create an instance of the type {type.Name}.");
+
+        return (IController)controller;
     }
 }
