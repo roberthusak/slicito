8c12876f87c07118bbf1f84959b6f0ba839da27f
Robert Husák <robert@husak.cloud>
2024-10-29 22:17:16 +0100

    Add strict type superset and subset checking operations

--- src/Slicito.Abstractions/Queries/FactTypeExtensions.cs
+++ src/Slicito.Abstractions/Queries/FactTypeExtensions.cs
@@ -2,8 +2,14 @@ namespace Slicito.Abstractions.Queries;
 
 public static class FactTypeExtensions
 {
+    public static bool IsStrictSupersetOf(this IFactType self, IFactType other) =>
+        !self.Equals(other) && self.IsSupersetOfOrEquals(other);
+
     public static bool IsSupersetOfOrEquals(this IFactType self, IFactType other) =>
         self.GetSmallestCommonSuperset(other).Equals(self);
+
+    public static bool IsStrictSubsetOf(this IFactType self, IFactType other) =>
+        !self.Equals(other) && self.IsSubsetOfOrEquals(other);
 
     public static bool IsSubsetOfOrEquals(this IFactType self, IFactType other) =>
         other.IsSupersetOfOrEquals(self);
