5c7e4dc3c81286aff83c814305c679021a1880ff
Robert Husák <robert@husak.cloud>
2024-10-31 15:46:15 +0100

    Add default values for optional properties of graph model

--- src/Slicito.Abstractions/Models/Graph.cs
+++ src/Slicito.Abstractions/Models/Graph.cs
@@ -4,6 +4,6 @@ namespace Slicito.Abstractions.Models;
 
 public sealed record Graph(ImmutableArray<Node> Nodes, ImmutableArray<Edge> Edges) : IModel;
 
-public sealed record Node(string Id, string? Label, Command? ClickCommand);
+public sealed record Node(string Id, string? Label = null, Command? ClickCommand = null);
 
-public sealed record Edge(string SourceId, string TargetId, string? Label, Command? ClickCommand);
+public sealed record Edge(string SourceId, string TargetId, string? Label = null, Command? ClickCommand = null);
