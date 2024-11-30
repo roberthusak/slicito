cfd906e0f013e99d02f744d997154a280b8f559f
Robert Husák <robert@husak.cloud>
2024-11-30 18:45:31 +0100

    Make helper class for sorts more precise

--- src/Slicito.ProgramAnalysis/SymbolicExecution/SmtLib/Sorts.cs
+++ src/Slicito.ProgramAnalysis/SymbolicExecution/SmtLib/Sorts.cs
@@ -2,17 +2,17 @@ namespace Slicito.ProgramAnalysis.SymbolicExecution.SmtLib;
 
 public static class Sorts
 {
-    public static Sort Bool { get; } = new Sort.Bool();
+    public static Sort.Bool Bool { get; } = new Sort.Bool();
 
-    public static Sort BitVec8 { get; } = new Sort.BitVec(8);
+    public static Sort.BitVec BitVec8 { get; } = new Sort.BitVec(8);
 
-    public static Sort BitVec16 { get; } = new Sort.BitVec(16);
+    public static Sort.BitVec BitVec16 { get; } = new Sort.BitVec(16);
 
-    public static Sort BitVec32 { get; } = new Sort.BitVec(32);
+    public static Sort.BitVec BitVec32 { get; } = new Sort.BitVec(32);
 
-    public static Sort BitVec64 { get; } = new Sort.BitVec(64);
+    public static Sort.BitVec BitVec64 { get; } = new Sort.BitVec(64);
 
-    public static Sort BitVec(int width) => width switch
+    public static Sort.BitVec BitVec(int width) => width switch
     {
         8 => BitVec8,
         16 => BitVec16,
