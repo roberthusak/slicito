c68d429ebbb92515943c4ca7ef360e8d66631b22
Robert Husák <robert@husak.cloud>
2024-12-06 18:24:43 +0100

    Add VS extension from template

---
+++ src/Slicito.VisualStudio/Commands/MyToolWindowCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Slicito.VisualStudio
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyToolWindowCommand : BaseCommand<MyToolWindowCommand>
+    {
+        protected override Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            return MyToolWindow.ShowAsync();
+        }
+    }
+}
---
+++ src/Slicito.VisualStudio/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Slicito.VisualStudio
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "My Tool Window";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("681e5668-1e63-46cf-881d-1ca8d6fc2e51")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
