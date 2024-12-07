b9303ec058220939a9db962be5e27e50ccc6432c
Robert Husák <robert@husak.cloud>
2024-12-07 01:40:53 +0100

    Update GUI of main window in VS

--- src/Slicito.VisualStudio/ToolWindows/MainWindowControl.xaml.cs
+++ src/Slicito.VisualStudio/ToolWindows/MainWindowControl.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -7,6 +8,7 @@ using Controllers;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Threading;
 
+using Slicito.Abstractions;
 using Slicito.Common;
 namespace Slicito.VisualStudio;
 
@@ -14,17 +16,30 @@ public partial class MainWindowControl : UserControl
 {
     private readonly SlicitoPackage _package;
 
+    private readonly List<Tuple<string, Func<IController>>> _controllerFactories =
+    [
+        new("Sample Structure Browser", () => new SampleStructureBrowser(new TypeSystem())),
+    ];
+
     public MainWindowControl(SlicitoPackage package)
     {
         InitializeComponent();
         _package = package;
+
+        _controllersComboBox.ItemsSource = _controllerFactories;
     }
 
-    private void OnButtonClick(object sender, RoutedEventArgs e) => _package.JoinableTaskFactory.RunAsync(OnButtonClickAsync);
+    private void OnCreateWindow(object sender, RoutedEventArgs e) => _package.JoinableTaskFactory.RunAsync(OnCreateWindowAsync);
 
-    private async Task OnButtonClickAsync()
+    private async Task OnCreateWindowAsync()
     {
-        var id = _package.ControllerRegistry.Register(new SampleStructureBrowser(new TypeSystem()));
+        if (_controllersComboBox.SelectedIndex < 0)
+        {
+            return;
+        }
+
+        var (name, factory) = _controllerFactories[_controllersComboBox.SelectedIndex];
+        var id = _package.ControllerRegistry.Register(factory());
 
         var window = await _package.FindToolWindowAsync(typeof(ControllerWindow.Pane), id, true, _package.DisposalToken);
         if (window is null || window.Frame is null)
@@ -37,4 +52,8 @@ public partial class MainWindowControl : UserControl
         var windowFrame = (IVsWindowFrame) window.Frame;
         Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
     }
+
+    private void OnOpenScript(object sender, RoutedEventArgs e) => throw new NotImplementedException();
+
+    private void OnRunScript(object sender, RoutedEventArgs e) => throw new NotImplementedException();
 }
