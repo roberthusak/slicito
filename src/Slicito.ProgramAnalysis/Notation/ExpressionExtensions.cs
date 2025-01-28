bf56944b3b0c7e90c21bf84ed1d145307f0401e7
Robert Husák <robert@husak.cloud>
2025-01-28 22:41:31 +0100

    Add extension method to obtain expression data type

--- src/Slicito.ProgramAnalysis/Notation/ExpressionExtensions.cs
+++ src/Slicito.ProgramAnalysis/Notation/ExpressionExtensions.cs
@@ -10,4 +10,42 @@ public static class ExpressionExtensions
             _ => expression == other,
         };
     }
+
+    public static DataType GetDataType(this Expression expression)
+    {
+        return expression switch
+        {
+            Expression.Constant.Boolean _ => DataType.Boolean.Instance,
+            Expression.Constant.SignedInteger signedInteger => signedInteger.Type,
+            Expression.Constant.UnsignedInteger unsignedInteger => unsignedInteger.Type,
+            Expression.Constant.Float @float => @float.Type,
+            Expression.VariableReference variableReference => variableReference.Variable.Type,
+            Expression.BinaryOperator binaryOperator => GetBinaryOperatorDataType(binaryOperator),
+            _ => throw new InvalidOperationException($"Expression of type '{expression.GetType().Name}' is not supported."),
+        };
+    }
+
+    private static DataType GetBinaryOperatorDataType(Expression.BinaryOperator binaryOperator)
+    {
+        return binaryOperator.Kind switch
+        {
+            BinaryOperatorKind.Add => binaryOperator.Left.GetDataType(),
+            BinaryOperatorKind.Subtract => binaryOperator.Left.GetDataType(),
+            BinaryOperatorKind.Multiply => binaryOperator.Left.GetDataType(),
+            BinaryOperatorKind.Divide => binaryOperator.Left.GetDataType(),
+            BinaryOperatorKind.Modulo => binaryOperator.Left.GetDataType(),
+            BinaryOperatorKind.And => DataType.Boolean.Instance,
+            BinaryOperatorKind.Or => DataType.Boolean.Instance,
+            BinaryOperatorKind.Xor => DataType.Boolean.Instance,
+            BinaryOperatorKind.ShiftLeft => binaryOperator.Left.GetDataType(),
+            BinaryOperatorKind.ShiftRight => binaryOperator.Left.GetDataType(),
+            BinaryOperatorKind.Equal => DataType.Boolean.Instance,
+            BinaryOperatorKind.NotEqual => DataType.Boolean.Instance,
+            BinaryOperatorKind.LessThan => DataType.Boolean.Instance,
+            BinaryOperatorKind.LessThanOrEqual => DataType.Boolean.Instance,
+            BinaryOperatorKind.GreaterThan => DataType.Boolean.Instance,
+            BinaryOperatorKind.GreaterThanOrEqual => DataType.Boolean.Instance,
+            _ => throw new InvalidOperationException($"Binary operator '{binaryOperator.Kind}' is not supported."),
+        };
+    }
 }
