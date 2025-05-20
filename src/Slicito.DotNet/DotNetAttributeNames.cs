582b9f5b33ec048d9060ac75ed79d8826119ade0
Robert Husák <robert@husak.cloud>
2025-05-20 16:55:04 +0200

    Add Kind to CommonAttributeNames

--- src/Slicito.DotNet/DotNetAttributeNames.cs
+++ src/Slicito.DotNet/DotNetAttributeNames.cs
@@ -4,7 +4,7 @@ namespace Slicito.DotNet;
 
 public static class DotNetAttributeNames
 {
-    public const string Kind = "Kind";
+    public const string Kind = CommonAttributeNames.Kind;
     public const string OperationKind = "OperationKind";
 
     public const string Name = CommonAttributeNames.Name;
