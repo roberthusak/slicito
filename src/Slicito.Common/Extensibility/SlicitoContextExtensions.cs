27114509f94b471238d8980c302d27064d30f9e7
roberthusak <robert@husak.cloud>
2025-07-15 19:37:18 +0200

    Move running scripts to Slicito.Common

--- src/Slicito.Common/Extensibility/SlicitoContextExtensions.cs
+++ src/Slicito.Common/Extensibility/SlicitoContextExtensions.cs
@@ -2,12 +2,39 @@ using System.Diagnostics.CodeAnalysis;
 
 using Slicito.Abstractions;
 using Slicito.Abstractions.Models;
+using Slicito.Common.Controllers;
 
 
 namespace Slicito.Common.Extensibility;
 
 public static class SlicitoContextExtensions
 {
+    [return: NotNullIfNotNull(nameof(value))]
+    public static IController? ConvertToController(this ISlicitoContext context, object? value)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+        else if (value is IController controller)
+        {
+            return controller;
+        }
+        else if (value is IModel model)
+        {
+            return new ModelDisplayer(model);
+        }
+        else if (context.TryCreateModel(value, out var createdModel))
+        {
+            return new ModelDisplayer(createdModel);
+        }
+        else
+        {
+            var textModel = new Tree([new(value.ToString(), [])]);
+            return new ModelDisplayer(textModel);
+        }
+    }
+
     public static bool TryCreateModel(this ISlicitoContext context, object value, [NotNullWhen(true)] out IModel? model)
     {
         var genericCreatorType = typeof(IModelCreator<>);
